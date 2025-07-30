0b45085cc3b8eb1d4f8bf5e90736bdbcdec25d26
PARIHARML <79474469+PARIHARML@users.noreply.github.com>
2025-07-30 08:41:13 +0530

    Array by ProCoder

---
+++ JavaScript Bootcamp/14.Multi Dimensional array/script.js
@@ -0,0 +1,18 @@
+// Multi Dimensional Array is also called array inside the array //
+
+const nameAndNumberList = [
+  ["Naveen", 75],
+  ["Nikhil", 80],
+  ["Adarsh", 60],
+];
+
+nameAndNumberList[0][1]; // 1st array ka 2nd element 
+nameAndNumberList[1][0]; // 2nd array ka 1st element
+
+nameAndNumberList[0][4] = "Mango"; // updating the 1st array
+
+const tikTacToeRespresentation = [
+  ["X", null, null],
+  [null, null, "O"],
+  ["O", null, "X",['Naveen',76,'Rewa']],
+];
