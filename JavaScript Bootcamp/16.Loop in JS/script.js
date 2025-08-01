3561bcab4877bea32a2169e84245c0648ebce12b
PARIHARML <79474469+PARIHARML@users.noreply.github.com>
2025-08-01 21:31:36 +0530

    Loop by ProCoder

---
+++ JavaScript Bootcamp/16.Loop in JS/script.js
@@ -0,0 +1,59 @@
+//while Loop- JS //
+// debugger
+// console.log("Chalo while Loop Seekhte Hain.....");
+
+// let i =0
+
+// while(i<=100){
+//  console.log(i);
+//  i++
+
+// }
+
+// const myFriends = ["Adarsh", "NAveen","Ravendra","Aakash","Atul",'Anand',"rahul",'Ravi','Nikhil']
+
+// i = 0
+
+// while( i < myFriends.length){
+// // console.log(`${i+1})`, myFriends[i]);
+// // console.log(`${i+1}) ${ myFriends[i]} proCoder`);
+// myFriends[0] = "proCoder"
+// i++
+
+// }
+
+// FOR LOOP //
+
+// for (let i = 0; i < 10 ; i++){
+
+//     console.log(`${i+1})`,myFriends[i]);
+//     console.log(`${i+1}`);
+//     console.log(i+1);
+
+// }
+
+// (WRITE A PROGRAM TO PRINT EVEN NUMBER BETWEEN 1 TO 100)
+// console.log("Chalo Even Number Print Katre Hain.....");
+
+// const printEvenNumber = +prompt("Enter number")
+
+// for(let i = 0 ; i<=printEvenNumber ; i+=2){ // variable i delete ho jata the program end hone ke baad but while and do while loop loop pe ye delete ni hota hai memory reserve karke rakhta because i globaly declear hua hota hai ( means i is global scoped not blocked scoped but for loop me i variable blocked scoped hota hai )//
+//     // if(i % 2 === 0){
+
+//         console.log( `${i} IS A EVEN NUMBER`);
+//         // console.log( "Naveen Mastering Frontend");
+//     // }
+
+// }
+
+// DO WHILE LOOP //
+
+console.log("LET'S LEARN DO WHILE LOOP");
+
+let i = 11;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
+
+console.log("program Ended!!");
