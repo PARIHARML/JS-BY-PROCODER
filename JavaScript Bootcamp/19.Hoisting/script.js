2f8f29463cc96524f097e62e0977f90cf1e48b1d
Naveen Singh Parihar <79474469+PARIHARML@users.noreply.github.com>
2025-08-03 20:39:24 +0530

    Function By ProCoder

---
+++ JavaScript Bootcamp/19.Hoisting/script.js
@@ -0,0 +1,24 @@
+
+// Very important Topic HoisTing in JavaScript //
+
+debugger
+
+    // console.log(a); // hoisted to hota  hai (const and let) ke case me v but TDZ(Temporal dead ZOne) me chala jata hai Js daal deta hai ,isliye error deta hai we cant access before initialization but Var ke case me TDZ me ni jata ha Isliye OUTPUT deta Undefine.
+
+    // var a = "NaveenSingh"
+
+    // hi()
+
+    // function hi(){  // Function declaration //
+
+    //     console.log("Hello World!!!");
+        
+    // }
+ 
+whoAreYou()
+
+var whoAreYou = function(){ // Function Expression because (= equal to sign laga hai) // // But Both Are Function Definition //
+
+    console.log("Nahi Batauga😂"); // without assigned function called anonymous function but when we assign into variable to usko ek Naam Mil jata Hai . // (IIFE => immediately invoked function expression )
+}
+
