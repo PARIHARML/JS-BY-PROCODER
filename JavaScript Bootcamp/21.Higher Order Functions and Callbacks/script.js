c828518451ad847b3cc96022be9d8d823612fde6
Naveen Singh Parihar <79474469+PARIHARML@users.noreply.github.com>
2025-08-04 23:31:50 +0530

    Function By ProCoder

---
+++ JavaScript Bootcamp/21.Higher Order Functions and Callbacks/script.js
@@ -0,0 +1,43 @@
+
+                                    // Higher Order Function //
+
+// function a(b){
+
+//     console.log( b, "kya Haal Chaal");
+    // console.log(typeof b, "iska type hai");
+    
+// }   
+// a('Nahi-Batauga')                               
+// a('chal hattt')                               
+// a({username: 'NaveenSingh', age: 25 , City: 'Rewa'})                               
+// a([8,4,6,2])  
+
+// console.log(a);
+// console.dir(a);  // function behind the seen object hi hota hai we (can know with the help of console.dir(a)) //
+// console.dir('Naveen');
+// console.dir(123654); // Number ek object nahi hota hai but jS me Number Naam se ek Object hota Hai . //
+
+// debugger
+
+function a(b){   // a is a function which is a higher order function because i am passing function inside it. isliye a ek higher order function ban chuka hai. //
+
+    // console.log( b);
+    console.dir(b);
+
+    b()  // call back Function - function ko hm directly call ni karte hain //
+}
+
+// function sayHi(){
+// console.log("Haaayiieeeee");
+
+
+// }
+// call back function process start from here.. when we call any function during thats time we pass another fun as an argument ,So passed fun called call back function //
+a(function (){                //  Anonymous Function - when we pass function without name as an argument  //
+console.log("Haaayiieeeee");  
+
+})
+
+// const x = sayHi   
+// x()
+// x.age = 25
