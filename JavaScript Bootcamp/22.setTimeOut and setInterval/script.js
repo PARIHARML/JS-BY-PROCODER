114773344274a6ab28300effebd1f015e9a35b13
Naveen Singh Parihar <79474469+PARIHARML@users.noreply.github.com>
2025-08-06 00:42:06 +0530

    Update script.js

--- JavaScript Bootcamp/22.setTimeOut and setInterval/script.js
+++ JavaScript Bootcamp/22.setTimeOut and setInterval/script.js
@@ -1,7 +1,7 @@
 
                                // SetTimeOut and setInterval //
 // debugger
-const timer1 = setTimeout('console.log("Hii-1")',1000) //1000 mili-second == 1sec// ye functionality hame browser pradaan krta hai ye javascript means ECMA Script me mention Nah hai AUR setTimeout ek (Higher order function hai) but ye Asynchronous code hai matlb ye memory me kahi jaakar save hota h pahle phir (print== run) hota hai. agr  //  
+const timer1 = setTimeout('console.log("Hii-1")',1000) //1000 mili-second == 1sec// ye functionality hame browser pradaan krta hai ye javascript means ECMA Script me mention Nah hai AUR setTimeout ek (Higher order function hai) but ye ek Asynchronous code v hai matlb ye memory me kahi jaakar save hota h pahle phir (print== run) hota hai. agr  //  
 
 // const timer4 = setInterval('console.log("Hii-1")',5000)// infinite time "Hii-1" print hota rahega bina koi Thakawat ke😂//
 
@@ -31,3 +31,4 @@ function a(){
 // a()
 
 console.log("main sab se pahle print houga setTimeout se pahle bhale hi setTimeout() me   time kuch v ho"); // beacuse i am synchronous code mera behavior (synchronous===one task at time) hai means == high priority //
+
