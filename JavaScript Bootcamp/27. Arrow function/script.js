ebd5cae9df70dbb7716d5ac5dd6ef11440267e80
Naveen Singh Parihar <79474469+PARIHARML@users.noreply.github.com>
2025-08-10 00:35:23 +0530

    FunctioN-BY-PROCODER

---
+++ JavaScript Bootcamp/27. Arrow function/script.js
@@ -0,0 +1,30 @@
+// 27. Arrow function
+
+// const addTwoNumber = (num1,num2) => ({username: "NaveenSingh"})
+// console.log(addTwoNumber());
+
+// function square(num){  // Function Declaration  //
+//     return num * num
+// }
+
+// const squareAnyNumber = function(num1,num2){  // Function Expression //
+//     return num1 ** num2
+// }
+
+// const squareAnyNumber = (num1,num2) => {  // Arrow Function Expression //
+//     return num1 ** num2
+
+// }
+
+// const numberOfTimesAnyNumber = (num1, num2) => num1 ** num2; // this is implicit{Automatic} return(num1 ** num2)
+
+// setTimeout(() => {   // function without passing argument //
+// // debugger
+//     console.log('Hyeee');
+
+// }, 2000)
+
+// const timer1 = setInterval(() =>  console.log('Hyeee'), 2000) // valid arrow function and anonymous function //
+
+random = () => (Math.floor(Math.random()*10)); // Explicit return //
+// const random = setInterval(() => console.log(Math.ceil(Math.random() * 10)),2000); // Explicit return //
